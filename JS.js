6302e5022d8e4085c25426a52baeb42b791c3456
dymjin <dymjin@gmail.com>
2023-08-03 12:23:21 +0200

    Edit styling to achieve proper grid

--- JS.js
+++ JS.js
@@ -1,7 +1,9 @@
+let gridSize;
 const gridContainer = document.querySelector('.grid-container');
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 400; i++) {
     const div = document.createElement('div');
     div.setAttribute('class', 'grid-box');
+    div.setAttribute('style', 'width: 20px; height: 20px');
 
     gridContainer.appendChild(div);
 }
