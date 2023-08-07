f80afa7fe1666648b270faefda93498e454e3d53
dymjin <dymjin@gmail.com>
2023-08-07 12:15:48 +0200

    Add new grid btn* + newGrid func* *new grid button generates a new grid with user input while removing the old grid *newGrid makes a new grid based on size parameter

--- JS.js
+++ JS.js
@@ -1,25 +1,43 @@
 const gridContainer = document.querySelector('.grid-container');
 const body = document.querySelector('body');
-const gridSize = Number(window.prompt('Enter a number', ''));
-const boxSize = gridContainer.clientHeight / gridSize;
 let draw = false;
-let i = 0;
-while (i < (Math.pow(gridSize, 2))) {
-    const div = document.createElement('div');
-    div.setAttribute('class', 'grid-box');
-    div.setAttribute('style', `width: ${boxSize}px; height: ${boxSize}px`);
-    gridContainer.appendChild(div);
-    div.addEventListener('click', () => {
-        if (draw === true) {
-            draw = false;
-        } else {
-            draw = true;
-        }
-    });
-    div.addEventListener('mouseover', () => {
-        if (draw === true) {
-            div.style.backgroundColor = 'black';
-        }
-    });
-    i++;
+let boxes = [];
+const newGridBtn = document.createElement('button');
+newGridBtn.textContent = 'New Grid';
+newGridBtn.setAttribute('class', 'new-grid');
+body.insertBefore(newGridBtn, gridContainer);
+
+let newGrid = (size) => {
+    let i = 0;
+    const boxSize = gridContainer.clientHeight / size;
+    while (i < (Math.pow(size, 2))) {
+        const div = document.createElement('div');
+        div.setAttribute('class', 'grid-box');
+        div.setAttribute('style', `width: ${boxSize}px; height: ${boxSize}px`);
+        gridContainer.appendChild(div);
+        boxes.push(div);
+        div.addEventListener('click', () => {
+            if (draw === true) {
+                draw = false;
+            } else {
+                draw = true;
+            }
+        });
+        div.addEventListener('mouseover', () => {
+            if (draw === true) {
+                div.style.backgroundColor = 'black';
+            }
+        });
+        i++;
+    }
 }
+
+newGridBtn.addEventListener('click', () => {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    const gridSize = Number(window.prompt('Enter a number', ''));
+    newGrid(gridSize);
+});
+
+newGrid(10);
