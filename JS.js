f33eab43aac97c34abf59285da363813ad0afb77
dymjin <dymjin@gmail.com>
2023-08-07 10:12:50 +0200

    Fix formula for gridSize calculation* *Now resizable grid is possible and accepts input from user upon new grid creation

--- JS.js
+++ JS.js
@@ -1,9 +1,16 @@
-let gridSize;
 const gridContainer = document.querySelector('.grid-container');
-for (let i = 0; i < 400; i++) {
+const body = document.querySelector('body');
+const gridSize = Number(window.prompt('Enter a number', ''));
+const boxSize = gridContainer.clientHeight / gridSize;
+let draw = false;
+let i = 0;
+while (i < (Math.pow(gridSize, 2))) {
     const div = document.createElement('div');
     div.setAttribute('class', 'grid-box');
-    div.setAttribute('style', 'width: 20px; height: 20px');
-
+    div.setAttribute('style', `width: ${boxSize}px; height: ${boxSize}px`);
     gridContainer.appendChild(div);
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'black';
+    });
+    i++;
 }
